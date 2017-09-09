906f20bcbf3a93961423e78c1fea79e533608c2d
Thibault Riviere <thibault.riviere@scality.com>
2017-09-09 14:51:32 +0200

    ft(dev): add utils funciton for standard deviation
    - Variance
    - BesselVariance
    - StdDev (compute the standard deviation)
    - BesselStdDev (compute the bessel standard deviation)

---
+++ deviation.go
@@ -0,0 +1,41 @@
+package stats
+
+import "math"
+
+// Variance is the mean of the sum of all square deviation
+func Variance(datas []float64) float64 {
+	avg := Mean(datas)
+	var ret float64 = 0
+	for i := 0; i < len(datas); i++ {
+		dist := datas[i] - avg
+		if dist != 0 {
+			ret += math.Pow(dist, 2)
+		}
+	}
+	if ret == 0 {
+		return 0.0
+	}
+	return ret / float64(len(datas))
+}
+
+// besselVariance generalize the variance for the population from a simple
+// of the population
+func BesselVariance(datas []float64) float64 {
+	avg := Mean(datas)
+	var ret float64 = 0
+	for i := 0; i < len(datas); i++ {
+		ret += math.Pow(datas[i]-avg, 2)
+	}
+	return ret / float64(len(datas)-1)
+}
+
+// StdDev is the standard deviation, the square root of the variance
+func StdDev(datas []float64) float64 {
+	return math.Sqrt(Variance(datas))
+}
+
+// Bessel Standard deviation generalize the standard deviation for the population
+// from a simple of the population
+func BesselStdDev(datas []float64) float64 {
+	return math.Sqrt(BesselVariance(datas))
+}
---
+++ deviation_test.go
@@ -0,0 +1,106 @@
+package stats
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func testVariance(name string, datas []float64, expected float64) {
+	Convey(name, func() {
+		So(Variance(datas), ShouldEqual, expected)
+	})
+}
+
+func testBesselVariance(name string, datas []float64, expected float64) {
+	Convey(name, func() {
+		So(BesselVariance(datas), ShouldEqual, expected)
+	})
+}
+
+func testStdDev(name string, datas []float64, expected float64) {
+	Convey(name, func() {
+		So(StdDev(datas), ShouldEqual, expected)
+	})
+}
+
+func testBesselStdDev(name string, datas []float64, expected float64) {
+	Convey(name, func() {
+		So(BesselStdDev(datas), ShouldEqual, expected)
+	})
+}
+
+func TestVariance(t *testing.T) {
+	Convey("Testing Bessel Variance", t, func() {
+		var ret float64
+		var datas []float64
+
+		ret = float64(0)
+		datas = []float64{}
+		testBesselVariance("Without values", datas, ret)
+
+		ret = float64(1)
+		datas = []float64{3, 4, 5}
+		testBesselVariance("with variance of 1", datas, ret)
+
+		ret = float64(0)
+		datas = []float64{1, 1, 1, 1}
+		testBesselVariance("with no variance", datas, ret)
+	})
+}
+
+func TestBesselVariance(t *testing.T) {
+	Convey("Testing Variance", t, func() {
+		var ret float64
+		var datas []float64
+
+		ret = float64(0)
+		datas = []float64{}
+		testBesselVariance("Without values", datas, ret)
+
+		ret = float64(1)
+		datas = []float64{3, 4, 5}
+		testBesselVariance("with variance of 1", datas, ret)
+
+		ret = float64(0)
+		datas = []float64{1, 1, 1, 1}
+		testBesselVariance("with no variance", datas, ret)
+	})
+}
+
+func TestStdDev(t *testing.T) {
+	Convey("Testing Stddev", t, func() {
+		var ret float64
+		var datas []float64
+
+		ret = float64(0)
+		datas = []float64{}
+		testStdDev("without value", datas, ret)
+
+		ret = float64(1)
+		datas = []float64{3, 5}
+		testStdDev("with stddev of 1", datas, ret)
+
+		ret = float64(0)
+		datas = []float64{1, 1, 1, 1}
+		testStdDev("with no stddev", datas, ret)
+	})
+}
+
+func TestBesselStdDev(t *testing.T) {
+	Convey("Testing bessel Stddev", t, func() {
+		var ret float64
+		var datas []float64
+
+		ret = float64(0)
+		datas = []float64{}
+		testBesselStdDev("without value", datas, ret)
+
+		ret = float64(1)
+		datas = []float64{3, 4, 5}
+		testBesselStdDev("with stddev of 1", datas, ret)
+
+		ret = float64(0)
+		datas = []float64{1, 1, 1, 1}
+		testBesselStdDev("with no stddev", datas, ret)
+	})
+}
